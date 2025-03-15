67a01a3281d1726cad41296b9a7e08035a8a9d3b
Jackson Lloyd <jackson@jbrlloyd.dev>
2025-03-15 12:57:57 +0000

    handle HTTP index request, dumb validation on HTTP protocol and return 404 otherwise

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,49 @@
-use std::net::TcpListener;
+use std::{fs, io::{BufRead, BufReader, Write}, net::{TcpListener, TcpStream}};
 
 fn main() {
     let listener = TcpListener::bind("localhost:7878").unwrap();
 
     for stream in listener.incoming() {
-        let stream = stream.unwrap();
+        match stream {
+            Ok(stream) => handle_connection(stream),
+            Err(error) => println!("Connection Error: {:?}", error),
+        }
+    }
+}
 
-        println!("Connection established!");
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&stream);
+    let tcp_request: Vec<_> = buf_reader
+        .lines()
+        .map(|result| result.unwrap_or(String::new()))
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    println!("Request: {tcp_request:#?}");
+
+    let request_line = tcp_request.first();
+
+    if request_line.is_none() || !request_line.unwrap().contains("HTTP") {
+        println!("Malformed request");
+    }
+
+    let request_line = request_line.unwrap();
+
+    if request_line == "GET / HTTP/1.1" {
+        let status_line = "HTTP/1.1 200 OK";
+        let content = fs::read_to_string("index.html").unwrap();
+        let length = content.len();
+        
+        let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{content}");
+
+        stream.write_all(response.as_bytes()).unwrap();
+    } else {
+        let status_line = "HTTP/1.1 404 NOT FOUND";
+        let content = fs::read_to_string("404.html").unwrap();
+        let length = content.len();
+        
+        let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{content}");
+
+        stream.write_all(response.as_bytes()).unwrap();
     }
 }
